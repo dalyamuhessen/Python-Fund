b432b8595367ea0897be61151c6d9bb93608053a
dalyamuhessen <daliamohisen@gmail.com>
2026-04-11 10:20:15 +0300

    Python

---
+++ hello_world.py
@@ -0,0 +1,23 @@
+# # 1. TASK: print "Hello World"
+print("Hello World")
+
+# # Run the file to test
+
+# # 2. print "Hello Noelle!" with the name in a variable
+name="Noelle"
+print("Hello" + name)
+print("Hello" , name)
+ 
+
+# # # 3. print "Hello 42!" with the number in a variable
+name = 42
+print("Hello", name)   
+print("Hello", str(name) + "!")  # with a comma
+print("Hello " + str(name) + "!") # with a +  
+ 
+# # 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I Love to eat {} and {}".format(fave_food1,fave_food2))        # with f string
+print(f"I love to eat {fave_food1} and {fave_food2}.")
+# # Run the file to test
